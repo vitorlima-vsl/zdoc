1607a402339db48fcd83b073e956f64f65bf2223
Vitor Lima <vitorsawada2002@gmail.com>
2025-03-03 03:12:49 -0300

    :sparkles: feat: sectionHero finalizada

---
+++ src/components/Sections/SectionHero.vue
@@ -0,0 +1,109 @@
+<script setup lang="ts">
+
+
+
+</script>
+
+<template>
+
+    <section class="bg-blue-darker  text-white py-14">
+        <div class="w-[1400px]  mx-auto mt-32 flex justify-between">
+            <div class="w-2/3 "> 
+                <h4><span class="text-blue-lighter mr-4">//</span>Encontre as Melhores Soluções Tecnológicas do Mercado.</h4>
+                <h1 class="text-6xl font-bold mt-4">Zdoc Aonde a Criatividade Encontra-se Com a <br> Tecnologia</h1>
+                <p class="text-lg mt-4">Possuímos Expertise em <span class="text-yellow">GESTÃO DE DOCUMENTOS</span> <br> e <span class="text-yellow">CONSULTORIA EM TECNOLOGIA DA INFORMAÇÃO</span></p>
+                <div class="mt-8 flex gap-4">
+                    <a class="bg-yellow text-gray-900 py-4 px-6  hover:bg-yellowHover transition-all duration-300" href="#">Saiba Mais</a>
+                    <a class=" text-white py-4 px-6  transition-all hover:bg-white/10
+                    relative
+                    after:absolute after:bottom-0 after:left-0 after:w-1/3 after:h-[1.5px] after:bg-yellow hover:after:w-full after:transition-all after:ease-out after:duration-300
+                    
+                    " href="#">Ver Todos Projetos</a>
+                </div>
+            </div>
+            <div class="parentSlider">
+                <div class="slider " style="--width: 500px; --height:504px;
+                --quantity: 6;">
+                    <div class="list">
+                        <div class="item " style="--position: 1;"><img class="w-[350px]" src="../../../public/imagens/contractStep1.svg" alt=""></div>
+                        <div class="item" style="--position: 2;"><img src="../../../public/imagens/contractStep2.svg" alt=""></div>
+                        <div class="item" style="--position: 3;"><img src="../../../public/imagens/contractStep3.svg" alt=""></div>
+                        <div class="item" style="--position: 4;"><img src="../../../public/imagens/contractStep4.svg" alt=""></div>
+                        <div class="item" style="--position: 5;"><img src="../../../public/imagens/contractStep5.svg" alt=""></div>
+                        <div class="item " style="--position: 6;"><img src="../../../public/imagens/contractStep6.svg" alt=""></div>
+                    </div>
+                </div>
+         </div>
+
+        </div>
+    </section>
+
+
+</template>
+
+<style scoped>
+
+.heroImage{
+    background-size: cover;
+    background-position: center;
+}
+.parentSlider{
+    width: 650px;
+    height: 504px;
+    position: relative;
+    /* border: 1px solid red; */
+
+}
+.slider{
+    overflow: hidden;
+    width: 650px;
+    height: 624px;
+    /* border: 3px solid rgb(23, 13, 114); */
+    position: absolute;
+    bottom: -120px;
+    /* height: var(--height); */
+
+
+}
+.slider .list{
+    
+    display: flex;
+    width: 100%;
+    min-width: calc(var(--width) * var(--quantity));
+    position: relative;
+ 
+}
+.slider .list .item{
+    width: var(--width);
+    height: var(--height);
+    position: absolute;
+    left: 100%;
+    animation: autoRun 36s infinite linear;
+    
+    animation-delay: calc((36s / var(--quantity)) * (var(--position) - 1) * -1);
+}
+.slider .list .item:nth-child(2n) {
+    /* background-color: brown; */
+    transform: translate(0, 15%);
+}
+.slider .list .item:nth-child(2n+1) {
+    /* background-color: rgb(207, 123, 123); */
+    transform: translate(0, -10%);
+}
+.slider .list .item img{
+    width: 100%;
+    position: absolute;
+    bottom:0;
+}
+@keyframes autoRun{
+    0%{
+        left: 100%;
+    }
+    100%{
+        left: calc(var(--width) * -1);
+    }
+}
+
+
+
+</style>
