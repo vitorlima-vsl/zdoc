a63b8c4bb1a24be3a19d38bb88a9f0aab5507ea6
Vitor Lima <vitorsawada2002@gmail.com>
2025-03-03 03:12:33 -0300

    :sparkles: feat: header feito

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+
+
+
+
+</script>
+
+<template>
+   <!-- eu quero uma tag html para informações de contato -->
+    <!-- <address class="fixed top-0  w-full justify-items-center bg-blue-darker text-white py-2">
+        <div class="w-[1400px] flex text-md justify-evenly ">
+            <p>Telefone: <a href="tel:+551112345678">(11) 1234-5678</a></p>
+            <p>E-mail: <a href="mailto:contato@empresaxyz.com">contato@empresaxyz.com</a></p>
+            <p>399 Rua Santo André. Aracaju/SE - CEP: 01234-567</p>
+        </div>
+    </address> -->
+
+    <header class="fixed top-0 left-1/2 -translate-x-1/2 flex justify-between items-center  w-[1400px] py-2 px-4 rounded-[4px] bg-white border-[1.5px] border-gray-200 mt-1 font-semibold text-gray-400">
+        <div>
+            <img class="w-28" src="../../../public/imagens/marca_zdoc_simp3.png" alt="">
+        </div>
+        <nav class="">
+            <ul class="flex gap-2 ">
+                <li class="text-gray-900">Home</li>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+            </ul>
+        </nav>
+       <a class="bg-yellow text-gray-800 py-4 px-6 rounded-4xl hover:bg-yellowHover transition-all" href="#">Contato</a>
+    </header>
+   
+
+
+</template>
+
+<style scoped>
+
+</style>
