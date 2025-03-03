624d8e328d109f467d385c2fa3ec0642c3e995bf
Vitor Lima <vitorsawada2002@gmail.com>
2025-03-03 03:12:17 -0300

    :sparkles: feat: app vue com header e primeira sectao importados

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Header from './components/Header/Header.vue'
+import SectionHero from './components/Sections/SectionHero.vue'
+
+
+</script>
+
+<template>
+
+  <Header />
+  <SectionHero />
+
+
+</template>
+
+<style scoped>
+
+</style>
