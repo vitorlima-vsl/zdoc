143af2c527b7c26d6a594f63f19045439a2c65bc
Vitor Lima <vitorsawada2002@gmail.com>
2025-03-04 03:13:21 -0300

    :sparkles: feat: criação do component subtitle

---
+++ src/components/Subtitle.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+    defineProps<{
+        title: string
+    }>()
+
+</script>
+
+
+<template>
+      <h4><span class="text-blue-lighter mr-4">//</span>{{ title }}</h4>
+</template>
