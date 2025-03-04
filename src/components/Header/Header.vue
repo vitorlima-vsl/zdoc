68875336b15424551291bfaf20b4460e8a03fa65
Vitor Lima <vitorsawada2002@gmail.com>
2025-03-04 03:13:32 -0300

    :lipstick: feat: adicionando z-index ao cabeçalho fixo

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -15,7 +15,7 @@
         </div>
     </address> -->
 
-    <header class="fixed top-0 left-1/2 -translate-x-1/2 flex justify-between items-center  w-[1400px] py-2 px-4 rounded-[4px] bg-white border-[1.5px] border-gray-200 mt-1 font-semibold text-gray-400">
+    <header class="fixed z-50 top-0 left-1/2 -translate-x-1/2 flex justify-between items-center  w-[1400px] py-2 px-4 rounded-[4px] bg-white border-[1.5px] border-gray-200 mt-1 font-semibold text-gray-400">
         <div>
             <img class="w-28" src="../../../public/imagens/marca_zdoc_simp3.png" alt="">
         </div>
