27ed983015f8c38dfb252bec8ec1ac7a7ae5fe1a
Vitor Lima <vitorsawada2002@gmail.com>
2025-03-19 11:07:29 -0300

    :sparkles: feat: criação do footer

---
+++ src/components/Layout/Footer.vue
@@ -0,0 +1,52 @@
+<template>
+        <footer class="w-full pb-2 bg-[#010E1A]">
+            <div class="max-w-[1400px] mx-auto  py-12 h-full">
+                <div class="flex justify-between items-center">
+                    <img class="w-64" src="/imagens/marca_zdoc_simp.svg" alt="">
+                        <div class="text-white flex gap-2">
+                            
+                             <div class="">
+                                <svg class="w-6 fill-yellow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.!--><path d="M48 432L336 256 48 80l0 352zM24.5 38.1C39.7 29.6 58.2 30 73 39L361 215c14.3 8.7 23 24.2 23 41s-8.7 32.2-23 41L73 473c-14.8 9.1-33.4 9.4-48.5 .9S0 449.4 0 432L0 80C0 62.6 9.4 46.6 24.5 38.1z"/></svg>
+                             </div>
+                    
+                                <div>
+                                    <h4 class="font-bold text-2xl  ">Menu:</h4>
+                                        <ul class="flex gap-2 flex-col pl-1 text-sm font-semibold text-gray-400">
+                                            <a href="#section-hero" class="hover:text-blue-lighter ">Inicio</a>
+                                            <a class="hover:text-blue-lighter " href="#section-services">Serviços</a>
+                                            <a class="hover:text-blue-lighter" href="#section-choseUs">Sobre</a>
+                                            <a class="hover:text-blue-lighter" href="#section-clients">Clientes</a>
+                                            <a class="hover:text-blue-lighter" href="#section-contact">Contato</a>
+                                        </ul>
+                                </div>
+                        </div>
+                      <div class="flex flex-col gap-6">
+                            <div class="text-white flex gap-2">
+                                <div class="">
+                                <svg class="w-6 fill-yellow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.!--><path d="M48 432L336 256 48 80l0 352zM24.5 38.1C39.7 29.6 58.2 30 73 39L361 215c14.3 8.7 23 24.2 23 41s-8.7 32.2-23 41L73 473c-14.8 9.1-33.4 9.4-48.5 .9S0 449.4 0 432L0 80C0 62.6 9.4 46.6 24.5 38.1z"/></svg>
+                                </div>
+                                <div>
+                                    <h4 class="font-bold text-2xl ">Contato:</h4>
+                                    <span class="text-sm font-semibold text-gray-400">Telefone: 07932590445 <br> Email: contato@zdoc.com.br
+                                    </span>
+                                </div>
+                        </div>
+                       <div class="text-white flex gap-2">
+                            <div class="">
+                               <svg class="w-6 fill-yellow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.!--><path d="M48 432L336 256 48 80l0 352zM24.5 38.1C39.7 29.6 58.2 30 73 39L361 215c14.3 8.7 23 24.2 23 41s-8.7 32.2-23 41L73 473c-14.8 9.1-33.4 9.4-48.5 .9S0 449.4 0 432L0 80C0 62.6 9.4 46.6 24.5 38.1z"/></svg>
+                            </div>
+                               <div>
+                                   <h4 class="font-bold text-2xl ">Endereço:</h4>
+                                   <span class="text-xs font-semibold text-gray-400">R. José Ramos da Silva, 228 - Sala 14 <br> 13 de Julho, Aracaju - SE - 49020-200</span>
+                               </div>
+                        </div>
+                    </div>
+                </div>
+              
+            </div>
+            <div class="w-full text-center">
+                    <span class="text-gray-400 text-sm font-semibold">Feito Por:   <a class="text-blue-lighter hover:text-blue-lighterHover" href="https://vitorlima-vsl.github.io/vitorlima-vsl/" target="_blank" >Vitor dos Santos Lima - Front-End</a></span>
+                   
+                </div>
+        </footer>
+</template>
