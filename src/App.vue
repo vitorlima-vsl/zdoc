bc0f72d27c46ef2e10d0b8c45a10bca2419f88dd
Vitor Lima <vitorsawada2002@gmail.com>
2025-03-19 11:06:55 -0300

    :sparkles: feat: adicionando secao budget e footer ao app

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,22 @@ import SectionHero from './components/Sections/SectionHero.vue'
 import SectionServices from './components/Sections/SectionServices.vue'
 import SectionChoseUs from './components/Sections/SectionChoseUs.vue'
 import SectionClients from './components/Sections/SectionClients.vue'
-
+import SectionBudget from './components/Sections/SectionBudget.vue'
+import Footer from './components/Layout/Footer.vue'
 </script>
 
 <template>
 
   <Header  />
-  <SectionHero key='#ect' />
+  <SectionHero  />
   <SectionServices></SectionServices>
   <SectionChoseUs/>
   <SectionClients />
+  <SectionBudget />
+  <Footer />
 </template>
 
 <style scoped>
-
+  
+  
 </style>
