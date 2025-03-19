d21f3dc615b394cb8082a7556e4882f0cc0657f3
Vitor Lima <vitorsawada2002@gmail.com>
2025-03-19 06:09:05 -0300

    :sparkles: feat: atualização das classes de cor no componente Card para melhorar a estética

--- src/components/UI/Cards/Card.vue
+++ src/components/UI/Cards/Card.vue
@@ -15,13 +15,13 @@ defineProps<{
 <template>
     <article class="w-[350px] h-[350px] bg-white p-4 rounded-2xl flex flex-col justify-between border-[1.5px] border-gray-200">
                 <div class="">
-                    <div class="size-20  bg-blue-lighter flex items-center justify-center p-1 rounded-2xl mb-4"> <img class="size-12" :src="'/imagens/' + img" alt=""> </div>
+                    <div class="size-20  bg-blue-darker flex items-center justify-center p-1 rounded-2xl mb-4"> <img class="size-12" :src="'/imagens/' + img" alt=""> </div>
                     <h3 class="text-2xl font-semibold ">{{title}}</h3>
                     <p :title="paragraph"
                         class=" text-gray-500 line-clamp-5 text-xl my-2">{{ paragraph }}
                     </p>
                 </div>
-                <div class="flex justify-start  items-center gap-3 text-blue-lighter hover:text-blue-lighterHover font-medium text-2xl transition-colors duration-150">
+                <div class="flex justify-start  items-center gap-3 text-blue-darker hover:text-blue-darkerHover font-medium text-2xl transition-colors duration-150">
 
                 <a class="peer "
                     href="#">Saiba Mais  </a> 
