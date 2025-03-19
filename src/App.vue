66d42b25e6dc725d6b7291e7270fb9d660dc703f
Vitor Lima <vitorsawada2002@gmail.com>
2025-03-19 06:07:36 -0300

    :sparkles: feat: adicionando secao clientes

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,17 @@ import Header from './components/Header/Header.vue'
 import SectionHero from './components/Sections/SectionHero.vue'
 import SectionServices from './components/Sections/SectionServices.vue'
 import SectionChoseUs from './components/Sections/SectionChoseUs.vue'
-
+import SectionClients from './components/Sections/SectionClients.vue'
 
 </script>
 
 <template>
 
-  <Header />
-  <SectionHero />
+  <Header  />
+  <SectionHero key='#ect' />
   <SectionServices></SectionServices>
   <SectionChoseUs/>
-
+  <SectionClients />
 </template>
 
 <style scoped>
